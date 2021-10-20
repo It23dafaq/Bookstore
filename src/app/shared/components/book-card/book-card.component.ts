ca0c283a99157d9118eba25417e573e6bd00e0c6
cfoulidis <cfoulidis@arx.gr>
2021-10-20 17:11:54 +0300

    create renew path and update object

--- src/app/shared/components/book-card/book-card.component.ts
+++ src/app/shared/components/book-card/book-card.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Book} from '../../../models/book.model';
 import {StorageService} from '../../../services/storageService/storage.service';
 import {Keys} from '../../../models/enums/keys.enum';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-book-card',
@@ -11,18 +12,26 @@ import {Keys} from '../../../models/enums/keys.enum';
 export class BookCardComponent implements OnInit {
   @Input() data?: Book | null | undefined ;
   @Input() isEnable = true;
+  @Input() isCategory = false;
   img = 'https://material.angular.io/assets/img/examples/shiba2.jpg';
   rating = '';
   tittle = 'test';
 
-  constructor(private storage: StorageService) { }
+  constructor(private storage: StorageService, private router: Router) { }
 
   ngOnInit(): void {
 
   }
   setSelectedItem(): void {
-
-    this.storage.setSelectedBook(Keys.selected_book, this.data as Book);
-
+    if (this.isEnable) {
+      this.storage.setSelectedBook(Keys.selected_book, this.data as Book);
+    }
+    if ( this.isCategory ){
+      this.storage.setSelectedBook(Keys.selected_book, this.data as Book);
+      const currentUrl = this.router.url;
+      this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+        this.router.navigate([currentUrl]);
+      });
+    }
   }
 }
