f48f8c45414c82f1ecda118f9e5b6c29ffa71857
cfoulidis <cfoulidis@arx.gr>
2021-10-22 19:03:06 +0300

    final Approach on reactive forms

--- src/app/home/components/book-form/book-form.component.ts
+++ src/app/home/components/book-form/book-form.component.ts
@@ -1,7 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ControlContainer, FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {firstLetterUppercaseValidator, forbiddenNameValidator} from '../../helpers/book-form-valitators';
-import {validate} from 'codelyzer/walkerFactory/walkerFn';
+
 
 @Component({
   selector: 'app-book-form',
@@ -9,44 +8,27 @@ import {validate} from 'codelyzer/walkerFactory/walkerFn';
   styleUrls: ['./book-form.component.css']
 })
 export class BookFormComponent implements OnInit {
-  bookFg!: FormGroup;
 
-  constructor(private fb: FormBuilder, public controlContainer: ControlContainer) { }
+  constructor(private fb: FormBuilder, public controlContainer: ControlContainer) {
+  }
 
-  get control(): FormGroup{
+  get control(): FormGroup {
     return this.controlContainer.control as FormGroup;
   }
+
   ngOnInit(): void {
-   this.bookFg = this.initBookInfo();
-   // this.bookFg.addControl('new-book', this.initBookInfo2());
-   this.control.addControl('bookFg', this.bookFg);
+    this.initBooks();
   }
 
-  addBook(): void{
-     this.bookFg.addControl('bookFg', this.initBookInfo());
-    // controls?.push(this.initBookInfo());
-    // this.control.addControl('new-book1', this.bookFg);
-  }
-  showDeleteBtn(index: number): void{
-    // return index > 0 || this.bookFg?.length === 1;
-  }
-  removeBook(index: number): void {
-    const controls = this.bookFg;
-   // controls?.removeAt(index);
-  }
-
-  initBookInfo(): FormGroup{
-    return this.fb.group({
-      title: new FormControl( null, [ Validators.maxLength(120), Validators.minLength(10), Validators.required] ),
-      description: new FormControl( '', [ Validators.required, Validators.maxLength(512), Validators.pattern(/^[A-Z].*$/)]),
-      author_name: new FormControl( '', [ Validators.required]),
-      publisher: new FormControl( '', [ Validators.maxLength(60), Validators.minLength(5), Validators.required]),
-      year: new FormControl( '', [ Validators.required]),
-      page_number: new FormControl( '', [ Validators.required]),
-      options: new FormControl( '', [ Validators.required]),
-      rating: new FormControl( '', [ Validators.required]),
-      isbn: new FormControl( '', [ Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
-      isbn_13: new FormControl( '', [ Validators.required, Validators.minLength(13), Validators.maxLength(13)]),
-    });
+  initBooks(): void {
+    this.control.addControl('title', new FormControl(null, [Validators.maxLength(120), Validators.minLength(10), Validators.required]));
+    this.control.addControl('description', new FormControl('', [Validators.required, Validators.maxLength(512), Validators.pattern(/^[A-Z].*$/)]));
+    this.control.addControl('author_name', new FormControl('', [Validators.required]));
+    this.control.addControl('publisher', new FormControl('', [Validators.maxLength(60), Validators.minLength(5), Validators.required]));
+    this.control.addControl('year', new FormControl('', [Validators.required]));
+    this.control.addControl('page_number', new FormControl('', [Validators.required]));
+    this.control.addControl('rating', new FormControl('', [Validators.required]));
+    this.control.addControl('isbn', new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]));
+    this.control.addControl('isbn_13', new FormControl('', [Validators.required, Validators.minLength(13), Validators.maxLength(13)]));
   }
 }
--- src/app/home/container/add-book/add-book.component.ts
+++ src/app/home/container/add-book/add-book.component.ts
@@ -1,8 +1,6 @@
-import {AfterViewInit, ChangeDetectionStrategy, Component, OnInit, ViewChild} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
-import {BookFormComponent} from '../../components/book-form/book-form.component';
-import {combineLatest, Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-book',
@@ -11,11 +9,27 @@ import {map} from 'rxjs/operators';
 })
 export class AddBookComponent implements OnInit {
   bookForm!: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  formArray = new FormArray([]);
+  formIsValid = false;
+  get controls(): FormGroup[]{
+   return this.formArray.controls as FormGroup[];
+  }
+  constructor(private fb: FormBuilder, private changeDetector: ChangeDetectorRef) { }
   ngOnInit(): void {
-    this.bookForm = this.fb.group({});
+    this.formArray.push(this.fb.group({}));
+    this.bookForm = this.fb.group({newBooks: this.formArray});
+    // Need timeout cuz error form validity changes bcz adding new form!
+    this.formArray.statusChanges.subscribe((status) => {
+      setTimeout(() => {
+        this.formIsValid = status === 'VALID';
+      }, 10);
+    });
   }
   onSubmit(): void {
     console.log(this.bookForm);
+  }
+  onAddnewBook(): void{
+     this.formArray.push(this.fb.group({}, ));
+     this.changeDetector.markForCheck();
   }
 }
