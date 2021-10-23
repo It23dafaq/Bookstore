f234214c4a2a57ad7aa8cab275ff493ec7406ff5
cfoulidis <cfoulidis@arx.gr>
2021-10-23 23:43:54 +0300

    fix error on isbn

--- src/app/home/components/book-form/book-form.component.ts
+++ src/app/home/components/book-form/book-form.component.ts
@@ -12,6 +12,7 @@ export const MY_FORMATS = {
     monthYearA11yLabel: 'YYYY',
   },
 };
+
 @Component({
   selector: 'app-book-form',
   templateUrl: './book-form.component.html',
@@ -36,10 +37,12 @@ export class BookFormComponent implements OnInit {
     this.control.addControl('description', new FormControl('', [Validators.required, Validators.maxLength(512), Validators.pattern(/^[A-Z].*$/)]));
     this.control.addControl('author_name', new FormControl('', [Validators.required, MaxWords(3)]));
     this.control.addControl('publisher', new FormControl('', [Validators.maxLength(60), Validators.minLength(5), Validators.required]));
-    this.control.addControl('year', new FormControl('', [Validators.required]));
-    this.control.addControl('page_number', new FormControl('', [Validators.required, MaxValidNumber(9999)]));
+    this.control.addControl('year', new FormControl('', [Validators.required, Validators.maxLength(4), Validators.min(0)]));
+    this.control.addControl('page_number', new FormControl('', [Validators.required, MaxValidNumber(9999), Validators.min(0)]));
     this.control.addControl('rating', new FormControl('', [Validators.required]));
-    this.control.addControl('isbn', new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]));
-    this.control.addControl('isbn_13', new FormControl('', [Validators.required, Validators.minLength(13), Validators.maxLength(13)]));
+    this.control.addControl('isbn', new FormControl('', [Validators.required, Validators.minLength(10),
+      Validators.maxLength(10), Validators.min(0)]));
+    this.control.addControl('isbn_13', new FormControl('', [Validators.required, Validators.minLength(13),
+      Validators.maxLength(13), Validators.min(0)]));
   }
 }
